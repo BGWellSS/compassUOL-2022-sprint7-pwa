37aee5c8746fa2321a532cba062e064232c4b427
Wellington Souza <wellington.souza.pb@compasso.com.br>
2022-11-17 15:32:52 -0300

    Theme typo and colors
    Theme typo and colors

--- src/styles/themes/theme.tsx
+++ src/styles/themes/theme.tsx
@@ -1,14 +1,34 @@
 // Page Theme
 const theme = {
   colors: {
-    background: {
-      primary: 'red',
-      secondary: 'blue',
-    },
+    primary: '#1B4B66',
+    primaryTint: '#639599',
+    error: '#B00020',
+    highlight: '#FF8C4B',
+    accent: '#F4F4F4',
+    dark: '#13101E',
+    bright: '#FFFFFF',
+    grey: '#F1F1F1',
+    lightText: '#B6B6B6',
+    highEmphasis: '#171520',
+    lowEmphasis: '#626262',
   },
   fonts: {
     familyPrimary: '"Inter", sans-serif;',
-    coreSize: '1.6rem',
+    baseSize: '1.6rem',
+  },
+  typos: {
+    displayLarge: 'font-size: 4rem; font-weight: 700; line-height: 5.2rem;',
+    displayMedium: 'font-size: 3.4rem; font-weight: 600; line-height: 4.4rem;',
+    displaySmall: 'font-size: 2rem; font-weight: 600; line-height: 2.6rem;',
+    titleMedium: 'font-size: 1.4rem; font-weight: 500; line-height: 1.8rem;',
+    titleRegular: 'font-size: 1.4rem; font-weight: 600; line-height: 1.6rem;',
+    bodyMedium: 'font-size: 1.6rem; font-weight: 500; line-height: 2rem;',
+    bodyRegular: 'font-size: 1.6rem; font-weight: 400; line-height: 2rem;',
+    labelLarge: 'font-size: 1.4rem; font-weight: 400; line-height: 2rem;',
+    labelMedium: 'font-size: 1.2rem; font-weight: 500; line-height: 1.6rem;',
+    labelSmall: 'font-size: 1.2rem; font-weight: 600; line-height: 1.6rem;',
+    link: 'font-size: 1.2rem; font-weight: 500; line-height: 1.8rem;',
   },
 };
 export default theme;
